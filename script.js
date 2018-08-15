02e5e3280f4f80c42c34897b9a7e5b28922977af
Dustin <dustin.tran@live.com>
2018-08-15 12:19:59 +0900

    completed script and working for previous 10 matches

--- script.js
+++ script.js
@@ -1,88 +1,174 @@
-// const d3 = require("d3");
 const axios = require("axios");
 
 async function getHeroes() {
-  const response = axios.get('https://api.opendota.com/api/heroes');
-  console.log(response);
+  const result = await axios.get('https://api.opendota.com/api/heroes');
+  return result;
 }
 
-// getHeroes();
 
+const MATCHES = 10; //number of matches to get
+
+async function getSecretLastFifty() {
+  const result = await axios.get('https://api.opendota.com/api/teams/1838315/matches');
+  const secretMatches = result.data.map((match) => {
+    return {
+      id: match.match_id,
+      radiant: match.radiant,
+    }
+  }).slice(0, MATCHES);
+  return secretMatches;
+}
+
+async function secretHeroes() {
+  const matchInfo = await getSecretLastFifty(); //calls function to get last matches
+  const promiseArray = [];
+  const apiStrings = matchInfo.map(match => ({ //converts matchInfo into api calls
+    api: `https://api.opendota.com/api/matches/${match.id}`
+  }));
+  apiStrings.forEach((item) => { //creates promise array of api calls
+    promiseArray.push(axios.get(item.api))
+  })
+  const promiseResults = await Promise.all(promiseArray); //returns an array of match details
+
+  //parse hero picks from promiseResults
+  //use match radiant val from matchInfo
+  const parsedInfo = promiseResults.map((match, index) => {
+    const heroPicks = [];
+    let team = 1;
+    if (matchInfo[index].radiant) {
+      team = 0;
+    }
+    match.data.picks_bans.forEach(turn => {
+      if (turn.is_pick && turn.team === team) {
+        heroPicks.push(turn.hero_id);
+      }
+    })
+    return heroPicks;
+  })
+  const heroes = await getHeroes();
+  const mergedInfo = [];
+  //check if mergedInfo has the hero in it
+  parsedInfo.forEach(match => {
+    match.forEach(heroId => {
+      let found = false;
+      if (mergedInfo.length > 0) {
+        for (let i = 0; i < mergedInfo.length; i++) {
+          if (mergedInfo[i].id === heroId) {
+            mergedInfo[i].Count++;
+            found = true;
+          }
+        }
+      }
+      //else, add the hero to the array
+      if (!found) {
+        let name;
+        heroes.data.forEach(hero => {
+          if (hero.id === heroId) {
+            name = hero.localized_name;
+          }
+        })
+        mergedInfo.push({
+          id: heroId,
+          Name: name,
+          Count: 1
+        })
+      }
+    })
+
+  })
+  console.log(mergedInfo);
+  return { children: mergedInfo };
+}
 ///d3 portion below///
 
-dataset = {
-  "children": [{
-    Name: "Rubick",
-    Count: "1000"
-  }]
-};
+/// test dataset ///
+// dataset = {
+//   "children": [{
+//     Name: "Rubick",
+//     Count: "1000"
+//   },
+//   {
+//     Name: "Lina",
+//     Count: "500"
+//   },
+//   {
+//     Name: "Lion",
 
-const diameter = 600;
-const color = d3.scaleOrdinal(d3.schemeCategory20);
+//     Count: "3200"
+//   }]
+// };
 
-const bubble = d3.pack(dataset)
-  .size([diameter, diameter])
-  .padding(1.5);
+async function runD3() {
+  const dataset = await secretHeroes();
 
-const svg = d3.select("body")
-  .append("svg")
-  .attr("width", diameter)
-  .attr("height", diameter)
-  .attr("class", "bubble");
+  const diameter = 600;
+  const color = d3.scaleOrdinal(d3.schemeCategory20);
 
-const nodes = d3.hierarchy(dataset)
-  .sum(function (d) { return d.Count; });
+  const bubble = d3.pack(dataset)
+    .size([diameter, diameter])
+    .padding(1.5);
 
-const node = svg.selectAll(".node")
-  .data(bubble(nodes).descendants())
-  .enter()
-  .filter(function (d) {
-    return !d.children
-  })
-  .append("g")
-  .attr("class", "node")
-  .attr("transform", function (d) {
-    return "translate(" + d.x + "," + d.y + ")";
-  });
+  const svg = d3.select("body")
+    .append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("class", "bubble");
 
-node.append("title")
-  .text(function (d) {
-    return d.Name + ": " + d.Count;
-  });
+  const nodes = d3.hierarchy(dataset)
+    .sum(function (d) { return d.Count; });
 
-node.append("circle")
-  .attr("r", function (d) {
-    return d.r;
-  })
-  .style("fill", function (d, i) {
-    return color(i);
-  });
+  const node = svg.selectAll(".node")
+    .data(bubble(nodes).descendants())
+    .enter()
+    .filter(function (d) {
+      return !d.children
+    })
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function (d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
 
-node.append("text")
-  .attr("dy", ".2em")
-  .style("text-anchor", "middle")
-  .text(function (d) {
-    return d.data.Name.substring(0, d.r / 3);
-  })
-  .attr("font-family", "sans-serif")
-  .attr("font-size", function (d) {
-    return d.r / 5;
-  })
-  .attr("fill", "white");
+  node.append("title")
+    .text(function (d) {
+      return d.Name + ": " + d.Count;
+    });
 
-node.append("text")
-  .attr("dy", "1.3em")
-  .style("text-anchor", "middle")
-  .text(function (d) {
-    return d.data.Count;
-  })
-  .attr("font-family", "Gill Sans", "Gill Sans MT")
-  .attr("font-size", function (d) {
-    return d.r / 5;
-  })
-  .attr("fill", "white");
+  node.append("circle")
+    .attr("r", function (d) {
+      return d.r;
+    })
+    .style("fill", function (d, i) {
+      return color(i);
+    });
 
-d3.select(self.frameElement)
-  .style("height", diameter + "px");
+  node.append("text")
+    .attr("dy", ".2em")
+    .style("text-anchor", "middle")
+    .text(function (d) {
+      return d.data.Name.substring(0, d.r / 3);
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", function (d) {
+      return d.r / 5;
+    })
+    .attr("fill", "white");
 
+  node.append("text")
+    .attr("dy", "1.3em")
+    .style("text-anchor", "middle")
+    .text(function (d) {
+      return d.data.Count;
+    })
+    .attr("font-family", "Gill Sans", "Gill Sans MT")
+    .attr("font-size", function (d) {
+      return d.r / 5;
+    })
+    .attr("fill", "white");
 
+  d3.select(self.frameElement)
+    .style("height", diameter + "px");
+
+}
+
+runD3();
